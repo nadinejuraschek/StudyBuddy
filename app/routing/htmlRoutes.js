aaaeb50dcf86f0a7a1a492e72baa97e3c0c1fc8d
Nadine Ju <nadine.jura@online.de>
2019-12-16 12:40:17 -0800

    html routes setup completed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,13 +3,11 @@ const path = require('path');
 
 module.exports = (app) => {
 
-    const pathname = req.url;
-
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname + '../public/survey.html'));
     });
 
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+    app.use(function(req, res) {
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
     });
 };
