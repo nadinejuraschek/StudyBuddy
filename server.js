e46499b019eea0b0d797cd7a0d30071a33175557
Nadine Ju <nadine.jura@online.de>
2019-12-16 12:30:52 -0800

    completed server setup

--- server.js
+++ server.js
@@ -7,12 +7,21 @@ const path = require('path');
 
 // NPM PACKAGES
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-// CUSTOM MODULES
-const htmlRoutes = require('./app/routing/htmlRoutes');
-const apiRoutes = require('./app/routing/apiRoutes');
+// listens either on PORT set by deployment or local PORT 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(8000, function() {
-    console.log("Server has started on PORT 8000.");
+// parse application
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// CUSTOM MODULES
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log("Server has started on PORT " + PORT + ".");
 });
