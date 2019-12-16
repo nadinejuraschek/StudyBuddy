b6fd6d3976875a622c5b47bd455292375758a3cc
Nadine Ju <nadine.jura@online.de>
2019-12-16 12:54:56 -0800

    dummy data added

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+{
+    "name": "Jason",
+    "photo": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "scores": []
+},
+{
+    "name": "Faith",
+    "photo": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    "scores": []
+},
+{
+    "name": "Kevin",
+    "photo": "https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "scores": []
+},
+{
+    "name": "Emily",
+    "photo": "https://images.pexels.com/photos/1541212/pexels-photo-1541212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "scores": []
+},
+{
+    "name": "Tracy",
+    "photo": "https://images.pexels.com/photos/1674752/pexels-photo-1674752.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "scores": []
+},
+{
+    "name": "Thomas",
+    "photo": "https://images.pexels.com/photos/733767/pexels-photo-733767.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "scores": []
+},
+{
+    "name": "Haley",
+    "photo": "https://images.pexels.com/photos/1903308/pexels-photo-1903308.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "scores": []
+},
+{
+    "name": "Mark",
+    "photo": "https://images.pexels.com/photos/2078265/pexels-photo-2078265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "scores": []
+}
